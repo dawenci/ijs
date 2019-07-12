709d93e4f70075ba3d883609063885806b393164
xwruan <xwruan@ccitc.net.cn>
2019-07-12 14:38:05 +0800

    新增单元测试

---
+++ test/addIndex.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const I = require('../dist/cjs')
+
+describe('addIndex', function() {
+  it('1 参数回调变 2 参数', function() {
+    const r1 = I.addIndex(I.map)((value, index) => `${value}-${index}`, ['A', 'B'])
+    assert.deepEqual(r1, ['A-0', 'B-1'])
+  })
+
+  it('2 参数回调变 3 参数', function() {
+    const reduceIndexed = I.addIndex(I.reduce)
+    const r2 = reduceIndexed((acc, value, index) => acc + (value + index), 0, [0,1,2])
+    assert.equal(r2, 6)
+  })
+})

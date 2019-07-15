fb00c57e09eea9a587a2e9828423f717e6ca500c
Davin Chang Juan <dawenci@gmail.com>
2019-07-16 00:05:02 +0800

    map 支持多列表

--- src/internal/_map.ts
+++ src/internal/_map.ts
@@ -1,9 +1,33 @@
-export default <E, R>(iteratee: (element: E) => R, list: ArrayLike<E>): Array<R> => {
-  list = list || []
-  const len = list.length || 0
-  const results = Array(len)
-  for (let index = 0; index < len; index += 1) {
-    results[index] = iteratee(list[index])
+const slice = Array.prototype.slice
+
+export default function <E, R>(iteratee: (element: E, ...rest) => R, coll: ArrayLike<E>): Array<R> {
+  coll = coll || []
+  switch (arguments.length) {
+    case 2: {      
+      const len = coll.length || 0
+      const results = Array(len)
+      for (let index = 0; index < len; index += 1) {
+        results[index] = iteratee(coll[index])
+      }
+      return results
+    }
+    case 3: {
+      const coll2 = arguments[2]
+      const len = Math.min(coll.length, coll2.length)
+      const results = Array(len)
+      for (let index = 0; index < len; index += 1) {
+        results[index] = iteratee(coll[index], coll2[index])
+      }
+      return results
+    }
+    default: {
+      const colls = slice.call(arguments, 1)
+      const len = Math.min.apply(Math, colls.map(coll => coll.length))
+      const results = Array(len)
+      for (let index = 0; index < len; index += 1) {
+        results[index] = iteratee.apply(void 0, colls.map(coll => coll[index]))
+      }
+      return results
+    }
   }
-  return results
 }
--- test/map.js
+++ test/map.js
@@ -2,7 +2,16 @@ const assert = require('assert')
 const I = require('../dist/cjs')
 
 describe('map', function() {
-  it('map', function() {
+  it('单列表', function() {
     assert.deepEqual(I.map(I.identity, [1,2,3]), [1,2,3])
+  })
+
+  it('多列表', function() {
+    assert.deepEqual(I.map(I.add, [1,2,3], [1,2,3]), [2,4,6])
+    assert.deepEqual(I.map((a,b,c)=>a+b+c, [1,2,3], [1,2,3], [1,2,3]), [3,6,9])
+  })
+
+  it('多列表，以最短列表为长度返回结果', function() {
+    assert.deepEqual(I.map((a,b,c)=>a+b+c, [1,2,3], [1,2], [1,2,3]), [3,6])
   })
 })

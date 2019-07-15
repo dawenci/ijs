ba35627b8bb8bd8c2d41781a2ae6ac52152530b3
xwruan <xwruan@ccitc.net.cn>
2019-07-15 18:33:01 +0800

    新增测试用例

--- src/merge.ts
+++ src/merge.ts
@@ -1,45 +1,5 @@
-const has = Object.prototype.hasOwnProperty
-
-export function _merge(output, to, from) {
-  const isOutputArray = Array.isArray(output)
-  if (to == null && from == null) return output
-  to = to == null ? isOutputArray ? [] : {} : Object(to)
-  from = from == null ? isOutputArray ? [] : {} : Object(from)
-
-  // 处理 to 的数据
-  for (let prop in to) {
-    const toVal = to[prop]
-
-    // 如果是 from 中没有该属性，使用 to 的值
-    if (!(prop in from)) {
-      output[prop] = toVal
-      continue
-    }
-
-    const fromVal = from[prop]
-
-    // 如果 from 中的属性值是原始类型（ 包括 undefined ），则直接覆盖
-    if ((typeof fromVal !== 'object' && typeof fromVal !== 'function') || fromVal === null) {
-      output[prop] = fromVal
-      continue
-    }
-
-    // 是否使用数组，取决于第一个对象的值是否为数组
-    const propOutput = Array.isArray(toVal) ? [] : {}
-
-    // 对象合并
-    output[prop] = _merge(propOutput, toVal, fromVal)
-  }
-
-  // 处理 from 的数据
-  for (let prop in from) {
-    // 上一轮遍历 from 时，已经合并的值，直接跳过
-    if (has.call(output, prop)) continue
-    output[prop] = from[prop]
-  }
- 
-  return output
-}
+import { curry2 } from './curry';
+import _merge from './internal/_merge'
 
 function merge(to, from) {
   return Array.isArray(to)
@@ -47,4 +7,4 @@ function merge(to, from) {
     : _merge({}, to, from)
 }
  
-export default merge
+export default curry2(merge)

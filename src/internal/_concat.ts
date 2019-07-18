506ed6600fad5ca1f45eb09ec21ff00c271d9ac9
xwruan <xwruan@ccitc.net.cn>
2019-07-18 18:51:25 +0800

    修改 concat 的实现，现在只接受两个参数

--- src/internal/_concat.ts
+++ src/internal/_concat.ts
@@ -1,9 +1,11 @@
-import _isArrayLike from './_isArrayLike'
+import _type from './_type'
 
-export default function concat(list1, list2, ...rest) {
-  if (typeof list1 === 'string' || Array.isArray(list1)) {
-    return list1.concat(list2, ...rest)
-  }
-  const result = []
-  return result.concat.apply(result, arguments)
+const isArgs = test => _type(test) === 'Arguments'
+const toArray = args => Array.prototype.slice.call(args)
+
+export default function concat(list1, list2) {
+  const result = typeof list1 === 'string' ? '' : []
+  if (isArgs(list1)) list1 = toArray(list1)
+  if (isArgs(list2)) list2 = toArray(list2)
+  return result.concat(list1, list2)
 }
--- test/concat.js
+++ test/concat.js
@@ -3,10 +3,14 @@ const I = require('../dist/cjs')
 
 describe('concat', function() {
   it('concat', function() {
-    assert.deepEqual(I.concat(1,2,3), [1,2,3])
-    assert.deepEqual(I.concat([1], 2, 3), [1,2,3])
-    assert.deepEqual(I.concat([1], [2], 3), [1,2,3])
-    assert.deepEqual(I.concat('1', 2, 3), '123')
+    assert.deepEqual(I.concat(1,2,3), [1,2])
+    assert.deepEqual(I.concat([1], 2, 3), [1,2])
+    assert.deepEqual(I.concat([1], [2]), [1,2])
+    assert.deepEqual(I.concat('1', 2), '12')
     assert.deepEqual(I.concat('1', '23'), '123')
+    function fn() {
+      return I.concat(arguments, arguments)
+    }
+    assert.deepEqual( fn(1), [1,1] )
   })
 })

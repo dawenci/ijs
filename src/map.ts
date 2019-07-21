64d8ad3122fe6f5cdb71e22d0bdd89f3901843ce
Davin Chang Juan <dawenci@gmail.com>
2019-07-21 21:18:50 +0800

    todo

--- src/map.ts
+++ src/map.ts
@@ -4,9 +4,18 @@ import _xmap from './internal/transducer/map'
 import _isTransformer from './internal/_isTransformer'
 import _isSequence from './internal/_isSequence'
 
+// TODO, 实现 map 协议
+const MAP = 'TODO'
+
 function map(fn, list) {
   // 若 list 为 Transformer，则用作 transducer
-  if (_isTransformer(list)) return _xmap(fn, list)
+  if (_isTransformer(list)) {
+    return _xmap(fn, list)
+  }
+
+  // if (list && typeof list[MAP] === 'function') {
+  //   return list[MAP](fn)
+  // }
 
   if (!_isSequence(list)) list = []
   return _map.apply(void 0, arguments)
